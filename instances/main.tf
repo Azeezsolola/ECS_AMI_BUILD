c07e69bad371b0662196381dfbf30cc1faf5d17e
Azeezsolola <oloyedeifeoluwa21@gmail.com>
2024-11-22 17:49:43 -0500

    Update main.tf

--- instances/main.tf
+++ instances/main.tf
@@ -96,12 +96,12 @@ data "aws_ami" "stack" {
 
   filter {
     name   = "name"
-    values = ["ami-stack-golden-image-01_ECS"]
+    values = ["ami-stack-golden-image-01_ECS3"]
   }
 }
 resource "aws_instance" "application_server" {
   ami                         = data.aws_ami.stack.id
-  instance_type               = "c5.4xlarge"
+  instance_type               = "c4.large"
   subnet_id                   = var.subnets[0]
   vpc_security_group_ids      = [aws_security_group.sg_22_80.id]
   associate_public_ip_address = true
