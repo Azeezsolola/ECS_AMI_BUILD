dc4d16aacde8140c2afa44d0f691fdfd2894ca0c
Azeezsolola <oloyedeifeoluwa21@gmail.com>
2024-11-22 18:33:30 -0500

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,12 +2,12 @@
 sudo yum update -y
 sudo yum install git -y
 sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
-sudo yum install -y httpd mariadb-server
-sudo systemctl start httpd
-sudo systemctl enable httpd
-sudo systemctl is-enabled httpd
+#sudo yum install -y httpd mariadb-server
+# sudo systemctl start httpd
+# sudo systemctl enable httpd
+# sudo systemctl is-enabled httpd
 sudo yum install -y nfs-utils
 sudo yum install amazon-cloudwatch-agent -y
 sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
-sudo systemctl restart ecs
-
+sudo amazon-linux-extras install docker -y
+sudo yum install mysql -y
