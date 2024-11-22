038f055b8ad8ec0fc73ed2b92f3464eea380e613
Azeezsolola <oloyedeifeoluwa21@gmail.com>
2024-11-21 20:58:50 -0500

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,3 +9,5 @@ sudo systemctl is-enabled httpd
 sudo yum install -y nfs-utils
 sudo yum install amazon-cloudwatch-agent -y
 sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+sudo systemctl restart ecs
+
